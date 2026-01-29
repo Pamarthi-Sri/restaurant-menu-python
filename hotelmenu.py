d2544910ed7671fce9736f837477ebecc5b1749f
PAMARTHI SRILAKSHMI <srilakshmipamarthi076@gmail.com>
2026-01-29 21:36:06 +0530

    Added restaurant menu Python project

---
+++ hotelmenu.py
@@ -0,0 +1,26 @@
+#define teh menu of restaurant
+menu={'pizza':40,
+'pasta':50,
+'Burger':60,
+'salad':70,
+'coffee':80
+}
+#Greet
+print("Welcome to PYTHON RESTAURANT")
+print("Pizza: Rs40\nPasta: Rs 50\nBurger:Rs 60\nSalad:Rs70\nCoffee:Rs80")
+order_total=0
+item_1=input("Enter the name of item you want to order:").lower()
+if item_1 in menu:
+    order_total+=menu[item_1]
+    print(f"Your item {item_1} has been added to your order")
+else:
+    print(f"Ordered item {item_1} is not available yet!")
+another_order=input("Do you want to add another item? (Yes/No)").lower()
+if another_order=="yes":
+    item_2=input("Enter name of the 2nd item=")
+    if item_2 in menu:
+        order_total+=menu[item_2]
+        print(f"Item {item_2} has been added to order")
+    else:
+        print(f"Ordered item {item_2} is not available!")
+print(f"The total amount of items to pay is {order_total}")
